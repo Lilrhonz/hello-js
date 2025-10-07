0c88b833d63fbc1e9a299ccc7c4cb83607e9e69b
Lilrhonz <lilybotwe4@gmail.com>
2025-10-07 14:04:36 +0000

    Update intro.js

--- intro.js
+++ intro.js
@@ -1,3 +1,4 @@
-console.log("My name is Lily Amakyewaa ."); // Name
+console.log("My name is Lily Amakyewaa Botwe."); // Name
 console.log("I am 18 years of age.");// Age
 console.log("Talking of my favorite programming language, I'm yet to find one."); // favorite programming language
+
