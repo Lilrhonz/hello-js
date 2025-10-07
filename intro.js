959eb604737d233f5e2569de77707e335450883a
Lilrhonz <lilybotwe4@gmail.com>
2025-10-07 13:09:21 +0000

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,3 @@
+console.log("My name is Lily Amakyewaa ."); // Name
+console.log("I am 18 years of age.");// Age
+console.log("Talking of my favorite programming language, I'm yet to find one."); // favorite programming language
